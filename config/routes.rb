55d70ddfb39c918e6fd98a5a4bd4313665a594ef
tianqizhao-louis <louiszhao0517@gmail.com>
2022-10-30 12:19:12 -0400

    route -> breeders

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rottenpotatoes::Application.routes.draw do
   resources :breeders
   resources :movies
   # map '/' to be a redirect to '/movies'
-  root :to => redirect('/movies')
+  root :to => redirect('/breeders')
 end
