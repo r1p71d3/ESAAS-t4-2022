e8bbd508c56977a43c54315ba8c9117286534734
tianqizhao-louis <louiszhao0517@gmail.com>
2022-10-31 16:58:27 -0400

    delete all movie stuff

--- db/schema.rb
+++ db/schema.rb
@@ -35,13 +35,4 @@ ActiveRecord::Schema.define(version: 20221030230549) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "movies", force: :cascade do |t|
-    t.string   "title"
-    t.string   "rating"
-    t.text     "description"
-    t.datetime "release_date"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
 end
